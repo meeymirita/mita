bd4ff0d959f2df4ca9bfe83985c0eaceaf8dbb51
mirita <popap182@gmail.com>
2026-01-07 23:14:42 +0300

    currentDate

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,11 @@
 <script setup>
-const colorRed = 'red'
-
-const user = {
-  "admin": {
-    "value": true,
-    "font": 25,
-  },
-}
-const isAdmin = user.admin.value
+const currentDate = new Date().toLocaleDateString()
 </script>
 
 <template>
-  <header>header</header>
-  <main
-    :style="{color:colorRed, fontSize: user.admin.font + 'px'}"
-    :class="`color-${colorRed}`"
-  >
-    {{isAdmin ? "adm" : "no" }}
-  </main>
-  <footer>{{10 + 20}}</footer>
+  <div>
+    {{ currentDate }}
+  </div>
 </template>
 
 <style scoped></style>
